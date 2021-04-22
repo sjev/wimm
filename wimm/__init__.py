cbd73a71f44cf0e618b3babe78d314fa2926ae37
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2021-04-22 18:41:07 +0200

    style

--- wimm/__init__.py
+++ wimm/__init__.py
@@ -1,33 +1,36 @@
+from pathlib import Path
+import os
+import sys
 __version__ = "DEV.0.0.8"
 
 DATE_FMT = "%Y-%m-%d"
 
-import sys
-import os
-from pathlib import Path
 
-this = sys.modules[__name__] # way to add module-wide variables
+this = sys.modules[__name__]  # way to add module-wide variables
+
 
 def get_path():
     """ get path of database directory """
-    
+
     val = os.getenv('WIMM_PATH')
 
     if not val:
         return None
     return Path(val)
 
-this.settings = {'path': get_path() }
+
+this.settings = {'path': get_path()}
+
 
 def get_settings():
     """ get settings from file or defaults """
-    
+
     import wimm.structure as structure
     path = this.settings['path']
-    
+
     if path is None:
         return structure.settings
-    
+
     p = path / structure.files['settings']
     if p.exists():
         import yaml
@@ -35,7 +38,6 @@ def get_settings():
         return settings
     else:
         return structure.settings
-        
-    
-    
+
+
 this.settings = {**this.settings, **get_settings()}
